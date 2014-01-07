4bda2fd8ce9f963e68f5388068f83567ff716acd
ryaweb <ryconle@gmail.com>
2014-01-07 17:11:19 -0500

    new update

--- com_braftonarticles/com_braftonarticles.script.php
+++ com_braftonarticles/com_braftonarticles.script.php
@@ -44,16 +44,5 @@ class com_braftonarticlesInstallerScript
 	 *
 	 * @param   JAdapterInstance  $adapter  The object responsible for running this script
 	 */
-	public function uninstall(JAdapterInstance $adapter) {
-		// Uninstalls s system plugin named plg_myplugin
-		$db = JFactory::getDBO();
-		$db->setQuery('SELECT `extension_id` FROM #__extensions WHERE `element` = "braftoncron" AND `folder` = "system"');
-		$id = $db->loadResult();
-		if($id)
-		{
-			$installer = new JInstaller;
-			$result = $installer->uninstall('plugin',$id,1);
-			$status->plugins[] = array('name'=>'plg_srp','group'=>'system', 'result'=>$result);
-		}
-	}
+
 }
