6fa53dd309548cdbcaa78ad0c5d12f7b5ab3a401
ryaweb <ryconle@gmail.com>
2014-03-05 14:03:41 -0500

    new function update

--- com_braftonarticles/admin/tables/categories.php
+++ com_braftonarticles/admin/tables/categories.php
@@ -50,7 +50,7 @@ class TableCategories extends JTable
 	 *
 	 * @return	int
 	 */
-	protected function _getAssetParentId($table = null, $id = null)
+	protected function _getAssetParentId(JTable $table = null, $id = null)
 	{
 		// We will retrieve the parent-asset from the Asset-table
 		$assetParent = JTable::getInstance('Asset');
